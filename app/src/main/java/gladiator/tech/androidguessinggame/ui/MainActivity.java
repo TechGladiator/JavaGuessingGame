9782daeba435dc5992e1a75a97cebf12df125116
John Stratton <jstrat0407@gmail.com>
2017-12-30 02:41:34 -0500

    updated imports

--- app/src/main/java/gladiator/tech/androidguessinggame/ui/MainActivity.java
+++ app/src/main/java/gladiator/tech/androidguessinggame/ui/MainActivity.java
@@ -3,6 +3,8 @@ package gladiator.tech.androidguessinggame.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import gladiator.tech.androidguessinggame.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
